4025f19f7bab7d49d39b1a0797076721808e5c30
unknown <remi4sure1@gmail.comYushau Balogungit config --global user.email remi4sure1@gmail.com>
2020-07-24 05:02:48 -0700

    Initial Project Version

---
+++ set.py
@@ -0,0 +1,25 @@
+# A Set is a collection which is unordered and unindexed. No duplicate members.
+
+# Create set
+fruits_set = {'Apples', 'Oranges', 'Mango'}
+
+# Check if in set
+print ('Apples' in fruits_set)
+
+# Add to set
+fruits_set.add ('Grape')
+
+# Remove from set
+fruits_set.remove ('Grape')
+
+
+# Add dublicate
+fruits_set.add ('Apples')
+
+# Clear set
+fruits_set.clear ()
+
+# Delete
+del fruits_set
+
+print (fruits_set)
---
+++ tuple.py
@@ -0,0 +1,22 @@
+# A Tuple is a collect which is ordered and unchangeable. Allows duplicate members.
+
+# Create tuple
+fruits = ('Apples', 'Oranges', 'Grapes')
+
+# Using a constructor
+# fruits2 = tuple (('Apples', 'Orange', Grapes))
+
+# Single value needs trailing comma
+fruits2 = ('Apples',)
+
+# Get value
+print (fruits [1])
+
+# Can't chnage value
+fruits [0] = 'Pears'
+
+# Delete tuple
+del fruits2
+
+# Get length
+print (len (fruits))
